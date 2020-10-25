b62f700aa33097e9d373de5cd32448fca9eea450
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-10-25 20:11:07 +1000

    Add edge-cases handling in `publication_history`

--- api/views.py
+++ api/views.py
@@ -1,11 +1,23 @@
-from django.http import JsonResponse
+from http import HTTPStatus
+
+from django.http import JsonResponse, HttpResponse
 
 from .science import get_publication_history
 
 
 def publication_history(request):
+    if request.method != "GET":
+        return HttpResponse(status=HTTPStatus.METHOD_NOT_ALLOWED)
+
     start_year = request.GET.get("start_year", 1965)
     end_year = request.GET.get("end_year", 2020)
+
+    try:
+        start_year = int(start_year)
+        end_year = int(end_year)
+    except ValueError:
+        payload = {"error": "`start_year` and `end_year` must be integer numbers."}
+        return JsonResponse(payload, status=HTTPStatus.BAD_REQUEST)
 
     history = get_publication_history(start_year, end_year)
     payload = []
@@ -17,5 +29,8 @@ def publication_history(request):
                 "publications": record.publications,
             }
         )
+
+    if not payload:
+        return HttpResponse(status=HTTPStatus.NO_CONTENT)
 
     return JsonResponse(payload, safe=False)
