93c98d1bfb21165f9730b2d62f9fffe278e24e7f
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-10-25 20:35:38 +1000

    Add OpenAPI spec in docstring for `publication_history`

--- api/views.py
+++ api/views.py
@@ -6,11 +6,63 @@ from .science import get_publication_history
 
 
 def publication_history(request):
+    """
+    paths:
+      /api/documents/history:
+        get:
+          operationId: getPublicationHistory
+          description: Получить историю публикаций.
+          parameters:
+            - in: query
+              name: startYear
+              schema:
+                type: integer
+                default: 1965
+              description: Начало исторического периода.
+            - in: query
+              name: endYear
+              schema:
+                type: integer
+                defaul: 2020
+              description: Конец исторического периода.
+          responses:
+              200:
+                description: История публикаций.
+                content:
+                  application/json:
+                    schema:
+                      type: array
+                      items:
+                        type: object
+                        properties:
+                          year:
+                            type: integer
+                            description: Год
+                          week:
+                            type: interger
+                            description: Неделя
+                          publications:
+                            type: intger
+                            description: Количество публикаций
+              204:
+                description: Публикации отсутствуют.
+              400:
+                description: Некорректный запрос.
+                content:
+                  application/json:
+                    schema:
+                      type: object
+                      properties:
+                        message:
+                          description: Сообщение об ошибке.
+                          type: string
+    """
+
     if request.method != "GET":
         return HttpResponse(status=HTTPStatus.METHOD_NOT_ALLOWED)
 
-    start_year = request.GET.get("start_year", 1965)
-    end_year = request.GET.get("end_year", 2020)
+    start_year = request.GET.get("startYear", 1965)
+    end_year = request.GET.get("endYear", 2020)
 
     try:
         start_year = int(start_year)
